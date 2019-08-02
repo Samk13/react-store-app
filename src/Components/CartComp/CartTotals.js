f027cd7f6059e992450a2f30a6ad3219f90ae76b
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2019-08-03 00:25:37 +0200

    fix warnings

--- src/Components/CartComp/CartTotals.js
+++ src/Components/CartComp/CartTotals.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import PayPalButton from './PayPalButton'
+//import { Link } from 'react-router-dom';
+//import PayPalButton from './PayPalButton'
 
 export default function CartTotals({ value, history }) {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
